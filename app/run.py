2cda61f1142fa2b07fb42be6fe22dd70034737cc
Saskia Kutz <skutz@zedat.fu-berlin.de>
2021-10-14 14:20:28 +0200

    added plot idea

--- app/run.py
+++ app/run.py
@@ -39,7 +39,7 @@ model = joblib.load("models/classifier.pkl")
 @app.route('/index')
 def index():
     # extract data needed for visuals
-    # TODO: Below is an example - modify to extract data for your own visuals
+    # TODO: Below is an example - modify to extract data for your own visuals -> number of for each category in dataset
     genre_counts = df.groupby('genre').count()['message']
     genre_names = list(genre_counts.index)
 
